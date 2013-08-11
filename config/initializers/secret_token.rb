1425d159b4a8f0746767a75b5b4cf630a4ae93d0
Ginger Griffis <gingergriffis@hotmail.com>
2013-08-11 15:20:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ginger::Application.config.secret_key_base = 'a661753884747decf14d6f241ed6b9fe1c5f64ccdf6a01ddc35d1c13baea923d7067492b88eada760bebd6530e87462e44305e199848ba44cdf19cde3feae689'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ginger::Application.config.session_store :cookie_store, key: '_ginger_session'
